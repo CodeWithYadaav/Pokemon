9798dbcf0a9d3ee301c75e02d02d764116645dae
Praveen Yadav <47134091+CodeWithYadaav@users.noreply.github.com>
2026-04-07 15:26:52 +0530

    Increase Pokémon API limit from 23 to 500

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -9,7 +9,7 @@ export const Pokemon = () => {
   const [error, setError] = useState(null)
   const [search, setSearch] = useState('')
 
-  const API = 'https://pokeapi.co/api/v2/pokemon?limit=23'
+  const API = 'https://pokeapi.co/api/v2/pokemon?limit=500'
 
   const fetchpokemon = async () => {
     try {
